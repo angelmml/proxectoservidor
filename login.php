12ec9483c67265912812e16394fc89060a7b8d2a
angelmml <128130405+angelmml@users.noreply.github.com>
2024-01-26 14:07:40 +0100

    sd

--- login.php
+++ login.php
@@ -1,12 +1,36 @@
 <?php
 session_start();
 
-if (isset($_POST["correo"]) && isset($_POST["password"]))
-{
-    $usuario = substr($_POST["correo"]);
-    $password = substr($_POST["password"]);
+if (isset($_POST["correo"]) && isset($_POST["contrasinal"])) {
+    $usuario = $_POST["correo"];
+    $password = $_POST["contrasinal"];
+
+    $mysqli = new mysqli("127.0.0.1", "root", "", "pedidos");
+
+    if ($mysqli->connect_error) {
+        die("La conexión a la base de datos ha fallado: " . $mysqli->connect_error);
+    }
+
+    $sql = "SELECT CodUsuario, Correo, Contrasinal, CodigoRol FROM usuario WHERE Correo = '$usuario'";
+    $result = $mysqli->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+
+        if ($password == $row["Contrasinal"]) {
+            $_SESSION["CodUsuario"] = $row["CodUsuario"];
+            $_SESSION["Correo"] = $row["Correo"];
+            $_SESSION["CodigoRol"] = $row["CodigoRol"];
+
+            header("Location: index.html");
+            exit();
+        } else {
+            echo "Contraseña incorrecta.";
+        }
+    } else {
+        echo "Usuario no encontrado.";
+    }
+
+    $mysqli->close();
 }
-
-$usuario = sub
-
-/*
+?>
