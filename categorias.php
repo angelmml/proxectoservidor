9d53d08191fe8b572be8a8c947d18e2d8b92eac4
angel <angelmartinezlago@gmail.com>
2024-01-29 23:40:00 +0100

    subida

--- categorias.php
+++ categorias.php
@@ -15,6 +15,7 @@
         <a class="active" href="categorias.php">Categorías</a>
         <a href="ofertas.php">Ofertas</a>
         <?php
+        session_start();
         // Verificar usuario iniciou sesión
         if (isset($_SESSION['correo'])) {
         // Sesion iniciada aparece "Área Personal" e pechar sesion
@@ -28,24 +29,48 @@
     </div>
     <div class="centro1">
         <h1>Categorías</h1>
+
+        <?php
+        //Conectamos a base de datos
+        $servername = "127.0.0.1";
+        $username = "root";
+        $password = "";
+        $dbname = "pedidos";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+        die("Error  na conexión a base de datos: " . $conn->connect_error);
+        }
+
+        //Gacemos a consulta
+        $query = "SELECT * FROM categoria WHERE Activo = 1";
+        $result = $conn -> query($query);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $nombreCategoria = $row['Nombre'];
+                $descripcionCategoria = $row['Descripcion'];
+
+                // Mostramos as caterogias
+                echo '<div class="categoria">';
+                echo '<h2>' . $nombreCategoria . '</h2>';
+                echo '<p>' . $descripcionCategoria . '</p>';
+                echo '</div>';
+                }
+            } else {
+                echo "Sen categorías dispoñibles";
+                    }
+
+                ?>
+
     </div>
 
-    <div class="mostracategorias">
-
-        <h3 id="carnes">Carnes</h3>        
-        <h3 id="peixes">Peixes</h3>
-        <h3 id="froitas_verduras">Froitas e Verduras</h3>
-        <h3 id="conxelados">Conxelados</h3>
-        <h3 id="pastas">Pastas e Arroces</h3>
-        <h3 id="panaderia">Panaderia & Bollería</h3>
-        <h3 id="snacks">Snaks e Chucherías</h3>
-        <h3 id="fogar">Fogar</h3>
-        <h3 id="hixiene">Hixiene</h3>
-        <h3 id="mascotas"></h3>
+  
 
     </div>
     <div class="footer">
-        <p>©MercaXallas 2024</p>
+        <p>© MercaXallas 2024</p>
     </div>
 </body>
 
